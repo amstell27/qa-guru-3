0a28e6f1f6eb8b2cd470185be0c90e2d0a294931
amstell27 <sergei_i27@mail.ru>
2022-02-21 21:02:05 +0700

    Add new project First Java

---
+++ src/main/java/guru/qa/Book.java
@@ -0,0 +1,12 @@
+package guru.qa;
+
+public class Book {
+
+    String author = "Дима Тучс";
+    String text = "Мурзилка";
+
+    void doReadBook (){
+        System.out.println("Книга прочитана! " + author);
+    }
+
+}
